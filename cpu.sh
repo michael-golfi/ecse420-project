5c3c2e27b7f4c62e5e54532909d0070ec4046bd6
Michael Golfi <michael.m.golfi@gmail.com>
2016-11-28 12:23:44 -0500

    Added tf install for cpu and deleted setup for cuda

---
+++ cpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Needs to be run as sudo
+
+apt-get install python-pip python-dev
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl
